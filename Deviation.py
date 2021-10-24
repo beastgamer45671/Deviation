8ea4632edfdddd266052eb76b4c754b778df5710
beastgamer45671 <71962379+beastgamer45671@users.noreply.github.com>
2021-10-24 17:28:09 +0530

    Update Deviation.py

--- Deviation.py
+++ Deviation.py
@@ -1,22 +1,22 @@
 import math
 import csv
 with open('data.csv', newline ='') as f:
-reader = csv.reader(f)
+  reader = csv.reader(f)
 file_data = list(reader)
 
 data = file_data[0]
 def mean(data):
-n = len(data)
+  n = len(data)
 total = 0
 for x in data:
-total += int(x)
-
+  total += int(x)
+  
 mean = total / n
 return mean
 
 squared_list = []
 for number in data:
-a = int(number) - mean(data)
+  a = int(number) - mean(data)
 a = a**2
 squared_list.append(a)
 
